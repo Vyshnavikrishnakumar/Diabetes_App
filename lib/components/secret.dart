5306e13e254c2137d99bd1a8c477f6d67418abac
Vyshnavikrishnakumar <165009740+Vyshnavikrishnakumar@users.noreply.github.com>
2025-02-20 11:31:59 +0530

    Update secret.dart

--- lib/components/secret.dart
+++ lib/components/secret.dart
@@ -1,6 +1,6 @@
 
 class Secrets {
-  static const String accountSid = "AC6432b23ad4f070143026379f070132db";
-  static const String authToken = "ce7a0b33135958adecbc09bca7b75946";
-  static const String twilioNumber = "+15074106535";
+  static const String accountSid = "ur acctid";
+  static const String authToken = "ur token";
+  static const String twilioNumber = "+number";
 }
